8e1557439e0c8c5f7b6c7af80eaca8229eac02d8
boipivo-hub <boipivo@gmail.com>
2026-07-11 20:32:47 +0200

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-firebase.auth().onAuthStateChanged(user => {
-    if (user) {
-        document.getElementById('auth-box').classList.add('hidden');
-        document.getElementById('forum-ui').classList.remove('hidden');
-        ForumNodes.loadPosts();
-    }
-});
