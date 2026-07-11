4504ca9151fc7ddb113755278d62a058b7c957c9
boipivo-hub <boipivo@gmail.com>
2026-07-11 14:15:31 +0200

    Update engine.js

--- engine.js
+++ engine.js
@@ -1,37 +1,10 @@
-// ARIES ROLE PLAY - SECURE ENGINE v10.0
-const firebaseConfig = { databaseURL: "https://aries-forum-default-rtdb.europe-west1.firebasedatabase.app/" };
-if (!firebase.apps.length) firebase.initializeApp(firebaseConfig);
-
-const App = {
-    user: null,
-    uid: null,
-    
-    // Инициализация безопасности
-    init() {
-        firebase.auth().onAuthStateChanged((user) => {
-            if (user) {
-                this.user = user.displayName;
-                this.uid = user.uid;
-            }
-            this.syncUI();
-        });
-    },
-
-    // Метод для безопасной записи (сервер проверит правила)
-    secureWrite(refPath, data) {
-        if (this.uid === '5694374929') {
-            firebase.database().ref(refPath).set(data);
-        } else {
-            console.error("Попытка взлома заблокирована.");
-        }
-    },
-
-    syncUI() {
-        // Логика отрисовки кнопок
-        const isOwner = (this.uid === '5694374929');
-        const admBtns = document.getElementById('adm-controls');
-        if (admBtns) admBtns.style.display = isOwner ? 'block' : 'none';
-    }
+const firebaseConfig = {
+  apiKey: "AIzaSyB3IcqqmojbVDiQaos8phPyWbzFCq0_TlM",
+  authDomain: "aries-forum.firebaseapp.com",
+  databaseURL: "https://aries-forum-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "aries-forum",
+  storageBucket: "aries-forum.firebasestorage.app",
+  messagingSenderId: "614643963857",
+  appId: "1:614643963857:web:01f1f941c72249ac6eb2f0"
 };
-
-App.init();
+firebase.initializeApp(firebaseConfig);
