c23446c1e3316c9333289a28fc83c979f2790e4e
boipivo-hub <boipivo@gmail.com>
2026-07-11 20:32:25 +0200

    Delete security.js

--- security.js
+++
@@ -1,7 +0,0 @@
-const Security = {
-    async hash(text) {
-        const msg = new TextEncoder().encode(text + "ARIES_PRIVATE_KEY_2026");
-        const buf = await crypto.subtle.digest('SHA-256', msg);
-        return Array.from(new Uint8Array(buf)).map(b => b.toString(16).padStart(2, '0')).join('');
-    }
-};
